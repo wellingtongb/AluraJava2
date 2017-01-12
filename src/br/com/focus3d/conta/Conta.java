0f54306415ea8f9b0cce23065fff5af0ebf6f388
Wellington <Wellington@Pc-Casa>
2017-01-11 23:17:44 -0200

    Finalização curso 3

--- src/br/com/focus3d/conta/Conta.java
+++ src/br/com/focus3d/conta/Conta.java
@@ -79,13 +79,13 @@ public abstract class Conta implements Comparable<Conta> {
 	}*/
 	
 	
-	/*@Override
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + numero;
 		return result;
-	}*/
+	}
 
 	@Override
 	public boolean equals(Object obj) {
